e2d46e6a1209ea992e7b9d7d24dc0a4a90f14e53
Shinnbeegpp <marfajonard17@gmail.com>
2025-04-14 20:55:41 +0800

    updated-tisting
    dasdasd

--- src/main/java/com/oopfinals/OOP/TISTING.java
+++ src/main/java/com/oopfinals/OOP/TISTING.java
@@ -3,4 +3,7 @@ package com.oopfinals.OOP;
 import ch.qos.logback.core.model.Model;
 
 public class TISTING {
+    public static void main(String[] args) {
+        System.out.println("bulalord");
+    }
 }
