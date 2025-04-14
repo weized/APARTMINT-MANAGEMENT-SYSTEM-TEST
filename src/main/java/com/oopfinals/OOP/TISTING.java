a25e367905c175209365474e74821e22ac57ccf6
Shinnbeegpp <marfajonard17@gmail.com>
2025-04-14 22:02:05 +0800

    commit2

--- src/main/java/com/oopfinals/OOP/TISTING.java
+++ src/main/java/com/oopfinals/OOP/TISTING.java
@@ -5,8 +5,6 @@ package com.oopfinals.OOP;
 public class TISTING {
     public static void main(String[] args) {
 
-        System.out.println("bulalord");
-        System.out.println("bulalord");
-        System.out.println("bulalord");
+        System.out.println("qwertyuiiop");
     }
 }
