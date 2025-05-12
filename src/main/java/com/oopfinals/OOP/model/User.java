b43481d1b11108d8e8da268255d9bbfd450172cd
dember marc <23-09786@g.batstate-u.edu.ph>
2025-05-12 12:28:40 +0800

    working-5-12-25

--- src/main/java/com/oopfinals/OOP/model/User.java
+++ src/main/java/com/oopfinals/OOP/model/User.java
@@ -12,59 +12,132 @@ import lombok.Setter;
 @Entity
 @Table(name = "tenants")
 public class User {
-    @Setter
-    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Getter
-    @Setter
+
     @Column(nullable = false, unique = true, length = 30)
     private String username;
 
-    @Getter
-    @Setter
+
     @Column(nullable = false, length = 25)
     private String middleName;
 
-    @Getter
-    @Setter
+
     @Column(nullable = false, length = 10)
     private String gender;
 
-    @Getter
-    @Setter
+
     @Column(nullable = false, length = 20)
     private String birthday;
 
-    @Getter
-    @Setter
     @Column(nullable = false, length = 11)
     private String phoneNumber;
 
-    @Getter
-    @Setter
+
     @Column(nullable = false, unique = true, length = 50)
     private String email;
 
-    @Getter
-    @Setter
+
     @Column(nullable = false, length = 64)
     private String password;
 
-    @Getter
-    @Setter
+
     @Column(nullable = false, length = 25)
     private String firstName;
 
-    @Getter
-    @Setter
     @Column(nullable = false, length = 25)
     private String lastName;
 
-    @Getter
-    @Setter
+
     @Column(nullable = false, length = 10)
     private String account_role;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAccount_role() {
+        return account_role;
+    }
+
+    public void setAccount_role(String account_role) {
+        this.account_role = account_role;
+    }
 }
